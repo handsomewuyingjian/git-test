34e28171672038653651200d28f661fcfd1135c5
吴英建 <1977693270@qq.com>
2023-01-28 10:37:16 +0800

    second commit

---
+++ src/main/java/com/wu/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.wu.git;
+
+/**
+ * @time: 2023/01/27 16:13
+ * @author: WYJ
+ * @description:
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git2");
+    }
+}
